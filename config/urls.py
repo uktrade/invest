acc1b897141253e6fcea431d3c3035fc7629f3c3
Stuart Axon <stuart.axon@digital.trade.gov.uk>
2018-04-30 10:35:32 +0100

    Remove unused import

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
-from django.http import HttpResponse
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
