e860f2fa4240d1851a07b62cf435107250d3056d
Alessandro De Noia <alessandro.denoia@digital.trade.gov.uk>
2018-05-09 16:15:29 +0100

    Redirect to language base on country mapping

--- invest/middleware.py
+++ invest/middleware.py
@@ -1,7 +1,10 @@
+from collections import defaultdict
 from urllib.parse import urlparse
 
 from django import http
 from django.conf import settings
+
+from django.http import HttpResponseRedirect
 from django.utils import translation
 from django.utils.encoding import uri_to_iri
 from django.utils.translation import check_for_language
@@ -9,6 +12,56 @@ from ipware.ip import get_real_ip
 from wagtail.contrib.redirects import models
 
 from .helpers import IPStackAPIClient
+
+
+COUNTRY_TO_LANGUAGE = defaultdict(lambda: 'en')
+COUNTRY_TO_LANGUAGE.update(
+    dict(
+        CN='zh',  # China
+        DE='de',  # Germany
+        JP='jp',  # Japan
+        FR='fr',  # France
+
+        # Arabic speaking
+        AE='ar',  # UAE
+        SA='ar',  # Saudi Arabia
+
+        # Portuguese speaking
+        BR='pt',  # Brazil
+        PT='pt',  # Portugal
+
+        # English speaking
+        GB='en',  # UK
+        US='en',  # USA
+        CA='en',  # Canada
+        AU='en',  # Australia
+        IN='en',  # India
+        NZ='en',  # New Zealand
+
+        # Spanish speaking
+        ES='es',  # Spain
+        MX='es',  # Mexico
+        CO='es',  # Colombia
+        AR='es',  # Argentina
+        PE='es',  # Peru
+        VE='es',  # Venezuela
+        CL='es',  # Chile
+        EC='es',  # Ecuador
+        GT='es',  # Guatemala
+        CU='es',  # Cuba
+        HT='es',  # Haiti
+        BO='es',  # Bolivia
+        DO='es',  # Dominican Republic
+        HN='es',  # Honduras
+        PY='es',  # Paraguay
+        NI='es',  # Nicaragua
+        SV='es',  # El Salvador
+        CR='es',  # Costa Rica
+        PA='es',  # Panama
+        PR='es',  # Puerto Rico
+        UY='es',  # Uruguay
+    )
+)
 
 
 class GeoIPLanguageMiddleware:
@@ -19,10 +72,16 @@ class GeoIPLanguageMiddleware:
     def __call__(self, request):
         client_ip = get_real_ip(request)
         if client_ip:
-            language = IPStackAPIClient.get_language(client_ip)
+            country_code = IPStackAPIClient.get_country_code(client_ip)
+            language = COUNTRY_TO_LANGUAGE[country_code]
             if check_for_language(language):
                 request.LANGUAGE_CODE = language
                 translation.activate(language)
+                link = '/{lang}{path}'.format(
+                    lang=language,
+                    path=request.path
+                )
+                return HttpResponseRedirect(link)
 
         response = self.get_response(request)
         return response
