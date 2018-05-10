8ddd593524898cc39ad88bf9ae118d64b85a6228
Alessandro De Noia <alessandro.denoia@digital.trade.gov.uk>
2018-05-10 09:45:08 +0100

    set s3 file overwrite to false

--- config/settings.py
+++ config/settings.py
@@ -162,6 +162,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_FILE_OVERWRITE = False
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
